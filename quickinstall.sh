2c46eb66557a4b4613b2445c1c079edbbed27ca0
root <ghr.ramos@gmail.com>
2018-05-18 07:55:34 -0300

    First commit

---
+++ quickinstall.sh
@@ -0,0 +1,158 @@
+#!/bin/sh
+
+umask 022
+
+which_cmd() {
+    which "${1}" 2>/dev/null || \
+        command -v "${1}" 2>/dev/null
+}
+
+check_cmd() {
+    which_cmd "${1}" >/dev/null 2>&1 && return 0
+    return 1
+}
+
+setup_terminal() {
+    TPUT_RESET=""
+    TPUT_BLACK=""
+    TPUT_RED=""
+    TPUT_GREEN=""
+    TPUT_YELLOW=""
+    TPUT_BLUE=""
+    TPUT_PURPLE=""
+    TPUT_CYAN=""
+    TPUT_WHITE=""
+    TPUT_BGBLACK=""
+    TPUT_BGRED=""
+    TPUT_BGGREEN=""
+    TPUT_BGYELLOW=""
+    TPUT_BGBLUE=""
+    TPUT_BGPURPLE=""
+    TPUT_BGCYAN=""
+    TPUT_BGWHITE=""
+    TPUT_BOLD=""
+    TPUT_DIM=""
+    TPUT_UNDERLINED=""
+    TPUT_BLINK=""
+    TPUT_INVERTED=""
+    TPUT_STANDOUT=""
+    TPUT_BELL=""
+    TPUT_CLEAR=""
+
+    # Is stderr on the terminal? If not, then fail
+    test -t 2 || return 1
+
+    if check_cmd tput
+    then
+        if [ $(( $(tput colors 2>/dev/null) )) -ge 8 ]
+        then
+            # Enable colors
+            TPUT_RESET="$(tput sgr 0)"
+            TPUT_BLACK="$(tput setaf 0)"
+            TPUT_RED="$(tput setaf 1)"
+            TPUT_GREEN="$(tput setaf 2)"
+            TPUT_YELLOW="$(tput setaf 3)"
+            TPUT_BLUE="$(tput setaf 4)"
+            TPUT_PURPLE="$(tput setaf 5)"
+            TPUT_CYAN="$(tput setaf 6)"
+            TPUT_WHITE="$(tput setaf 7)"
+            TPUT_BGBLACK="$(tput setab 0)"
+            TPUT_BGRED="$(tput setab 1)"
+            TPUT_BGGREEN="$(tput setab 2)"
+            TPUT_BGYELLOW="$(tput setab 3)"
+            TPUT_BGBLUE="$(tput setab 4)"
+            TPUT_BGPURPLE="$(tput setab 5)"
+            TPUT_BGCYAN="$(tput setab 6)"
+            TPUT_BGWHITE="$(tput setab 7)"
+            TPUT_BOLD="$(tput bold)"
+            TPUT_DIM="$(tput dim)"
+            TPUT_UNDERLINED="$(tput smul)"
+            TPUT_BLINK="$(tput blink)"
+            TPUT_INVERTED="$(tput rev)"
+            TPUT_STANDOUT="$(tput smso)"
+            TPUT_BELL="$(tput bel)"
+            TPUT_CLEAR="$(tput clear)"
+        fi
+    fi
+
+    return 0
+}
+setup_terminal || echo >/dev/null
+
+logging() {
+    echo >&2 " --- ${TPUT_DIM}${TPUT_BOLD}${*}${TPUT_RESET} --- "
+}
+
+run_ok() {
+    printf >&2 "${TPUT_BGGREEN}${TPUT_WHITE}${TPUT_BOLD} OK ${TPUT_RESET} ${*} \n\n"
+}
+
+run_failed() {
+    printf >&2 "${TPUT_BGRED}${TPUT_WHITE}${TPUT_BOLD} FAILED ${TPUT_RESET} ${*} \n\n"
+}
+
+fatal() {
+    printf >&2 "${TPUT_BGRED}${TPUT_WHITE}${TPUT_BOLD} ABORTED ${TPUT_RESET} ${*} \n\n"
+    exit 1
+}
+
+install() {
+	logging "Installing " + "${1}"
+	apt-get -y install "${1}"
+	local ret=$?
+
+	if [ ${ret} -ne 0 ]
+        then
+        run_failed
+        printf >> "${run_logfile}" "FAILED with exit code ${ret}\n"
+    else
+        run_ok
+        printf >> "${run_logfile}" "OK\n"
+}
+logging "Checking the latest version of static build..."
+
+logging "Checking the latest version of static build..."
+
+logging "Installing netdata"
+
+run_ok
+
+run_failed
+
+fatal
+
+apt-get -y install gcc
+apt-get -y install build-essential
+apt-get -y install make
+apt-get -y install tcpdump
+apt-get -y install net-tools
+apt-get -y install linux-headers-$(uname -r)
+apt-get -y install git
+apt-get -y install libtool
+apt-get -y install pkg-config
+apt-get -y install libbz2-dev
+apt-get -y install autoconf
+apt-get -y install bison
+apt-get -y install flex
+apt-get -y install librrd-dev
+
+git clone https://github.com/phaag/nfdump.git
+
+perl -MCPAN -e 'install Socket6'
+
+cd nfdump/
+
+chmod +x autogen.sh
+
+mkdir m4
+
+ACLOCAL_FLAGS="-I /usr/share/aclocal-1.15" ./autogen.sh
+
+./configure --enable-nfprofile --enable-nftrack
+
+make && make install
+
+useradd  -G www-data -m -s /bin/false netflow
+
+#nfcapd -w -D -p 9996 -u netflow -g www-data -S 1 -z -I ASA5515 -l /data/nfcap/ASA5515
+
